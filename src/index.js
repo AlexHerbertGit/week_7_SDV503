a4433b53bb35be251ed2e1a6cd61ed76f36d6d36
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-04-03 12:09:17 +1200

    changed the values to fit the question

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // Create a function that takes an array with only numbers and returns the first element.
  
     function myArrFunc(arr) {
-        return arr[1][2]
+        return arr[0][0]
     }
 
 console.log(myArrFunc([["Alex"],[1, 2, 3, 4, 5]]))
 //Simple Function calling 2D array.
+
