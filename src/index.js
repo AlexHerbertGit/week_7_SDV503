6be5ec130b480baebb06fa3bd1e076032c9eaf8e
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-04-03 12:15:26 +1200

    added function that increments a value by 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@
         return arr[0][0]
     }
 
-console.log(myArrFunc([["Alex"],[1, 2, 3, 4, 5]]))
+console.log(myArrFunc([["Alex"],[1, 2, 3, 4, 5]]));
 //Simple Function calling 2D array.
 
+//Create a function that takes a number, increment the number by 1 and return the result
+
+function inc(x) {
+   return x += 1
+}
+//Function takes the value of x adds 1 to it and returns the new value.
+console.log(inc(1))
+
+//Create a function that takes and integer minutes and convert it to seconds.
