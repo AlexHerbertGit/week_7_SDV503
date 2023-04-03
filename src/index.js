1e0e0059479a66e9355879b9620b585be378598e
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-04-03 12:04:02 +1200

    Added simple function calling 2D array

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Create a function that takes an array with only numbers and returns the first element.
+ 
+    function myArrFunc(arr) {
+        return arr[1][2]
+    }
+
+console.log(myArrFunc([["Alex"],[1, 2, 3, 4, 5]]))
+//Simple Function calling 2D array.
